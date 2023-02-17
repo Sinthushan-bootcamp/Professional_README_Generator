a0f62b2c0c26f5891118238f73267f7e008ef92b
sinthushan <sinthushan@gmail.com>
2023-02-17 18:38:38 -0500

    added question prompts and imported necessary requirements

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
-// TODO: Include packages needed for this application
-
+// requirements
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./util/generateMarkdown.js');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {type: 'input', message: "What is your GitHub username?", name: 'name'},
+    {type: 'input', message: "What is your email address?", name: 'name'},
+    {type: 'input', message: "What is your project's name?", name: 'name'},
+    {type: 'input', message: "Please write a short description of your project", name: 'name'},
+    {type: 'list', message: "Select the license your project should have?", name: 'name'},
+    {type: 'input', message: "What command should be run to install dependencies?", name: 'name'},
+    {type: 'input', message: "what command should be run to run tests?", name: 'name'},
+    {type: 'input', message: "What does the user need to know about using the repo?", name: 'name'},
+    {type: 'input', message: "What does the user need to know about contributing to the repo?", name: 'name'}
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
+
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+
+}
 
 // Function call to initialize app
 init();
